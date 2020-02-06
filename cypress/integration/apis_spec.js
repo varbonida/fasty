836a5be786c4210b814d94bdc8305756f1802f26
Vergel <vergel.arbonida@gmail.com>
2020-02-07 00:27:06 +0800

    ci

--- cypress/integration/apis_spec.js
+++ cypress/integration/apis_spec.js
@@ -1,5 +1,5 @@
 
-var host = 'http://demo.127.0.0.1.xip.io:8080/static/admin/'//'http://test.127.0.0.1.xip.io:8080'
+var host = 'http://localhost:40225/static/admin/'//'http://demo.127.0.0.1.xip.io:8080/static/admin/'//'http://test.127.0.0.1.xip.io:8080'
 describe('Core tests', function () {
 
   beforeEach(function () {
